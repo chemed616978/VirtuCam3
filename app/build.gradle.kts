328971e51b3329caba947e095430d67c81146683
chemed616978 <labasghaya52@gmail.com>
2025-10-29 12:41:23 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -36,34 +35,34 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-    // Android & Kotlin basics
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    // IjkPlayer (لتشغيل الفيديو)
+    // مكتبات IJKPlayer (مشغل الفيديو)
     implementation("tv.danmaku.ijk.media:ijkplayer-java:0.8.8")
     implementation("tv.danmaku.ijk.media:ijkplayer-armv7a:0.8.8")
     implementation("tv.danmaku.ijk.media:ijkplayer-arm64:0.8.8")
 
-    // Koin (لـ Dependency Injection)
-    implementation("io.insert-koin:koin-android:3.5.0")
-    implementation("io.insert-koin:koin-core:3.5.0")
+    // Xposed API (من مستودع api.xposed.info)
+    implementation("de.robv.android.xposed:api:82")
 
-    // Xposed (مطلوبة لملفات VideoToFrames و MainHook)
-    compileOnly("de.robv.android.xposed:api:82")
+    // مكتبة Koin لإدارة الاعتماديات (إذا كان المشروع يستخدمها)
+    implementation("io.insert-koin:koin-android:3.4.3")
 
-    // FFmpeg / JavaCV (في حال يستخدمها المشروع لتحليل الفيديوهات)
-    implementation("org.bytedeco:javacv:1.5.9")
-    implementation("org.bytedeco:ffmpeg:6.0-1.5.9")
+    // Firebase (اختياري)
+    implementation("com.google.firebase:firebase-analytics-ktx")
 
-    // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+``
