ec2d62841faec56b4acdfc1ac0869c7fd8c0d404
chemed616978 <labasghaya52@gmail.com>
2025-10-29 12:57:25 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") // Firebase
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,21 +46,13 @@ android {
     }
 }
 
-repositories {
-    google()
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven("https://maven.aliyun.com/repository/public")
-    maven("https://maven.bilibili.com/repository/public") // ijkplayer source
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
-    // IJKPlayer (video player)
+    // IJKPlayer
     implementation("tv.danmaku.ijk.media:ijkplayer-java:0.8.8")
     implementation("tv.danmaku.ijk.media:ijkplayer-armv7a:0.8.8")
     implementation("tv.danmaku.ijk.media:ijkplayer-arm64:0.8.8")
@@ -68,14 +60,14 @@ dependencies {
     // Firebase Analytics
     implementation("com.google.firebase:firebase-analytics-ktx:21.5.0")
 
-    // Lifecycle & ViewModel
+    // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
 
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
 
-    // Testing
+    // Tests
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
