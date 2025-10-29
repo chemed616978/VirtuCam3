0c7067d0902a7f4775e754fdafd52806fae1d7d9
chemed616978 <labasghaya52@gmail.com>
2025-10-29 12:39:38 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,18 +5,19 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         gradlePluginPortal()
         mavenCentral()
+        // مصدر Xposed
         maven("https://api.xposed.info/")
+        // jcenter لم يعد مدعوم، لكن سنبقيه للاحتياط
         jcenter()
-        maven ("https://maven.pkg.github.com/GCX-HCI/tray" )
     }
 }
 
 rootProject.name = "VCAMSX"
 include(":app")
- 
